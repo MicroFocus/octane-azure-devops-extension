4269cf5d4f32b5bbc57b556b0119a22a243331b7
lokshin <evgene.lokshin@microfocus.com>
2019-08-12 15:38:38 +0300

    Start using Azure WebApi.

--- Tasks/PipelineStartTask/dto/scm/ScmTestApi.ts
+++ Tasks/PipelineStartTask/dto/scm/ScmTestApi.ts
@@ -1,17 +1,18 @@
 import {ConnectionUtils} from "../../ConnectionUtils";
 import {ScmBuilder} from "./ScmBuilder";
+import {WebApi} from "azure-devops-node-api";
 
 let projectName = 'demo-app';
 
 // let token: string = process.env.AZURE_PERSONAL_ACCESS_TOKEN;
 const token = 'fzhzniawld2wh524y2h2sft2ksm23nanspwk6blg4lxhegirixcq';
 const orgUrl = 'https://dev.azure.com/evgenelokshin';
-let toBuild: number = 35;
+let toBuild: number = 34;
 
-ConnectionUtils.getAzureDevopsConnection(token, orgUrl).then(con => {
-    ScmBuilder.buildScmData(con, projectName, toBuild).then(scm => {
-        console.log('########################## finished ###############################');
-        console.log(scm);
-    })
+let api: WebApi = ConnectionUtils.getWebApiWithProxy(orgUrl, token);
+
+ScmBuilder.buildScmData(api, projectName, toBuild).then(scm => {
+    console.log('########################## finished ###############################');
+    console.log(scm);
 });
 
