1c8fa50f2397bf09290f95d47594519446248449
lokshin <evgene.lokshin@microfocus.com>
2019-08-12 15:17:23 +0300

    Start using Azure WebApi.

--- Tasks/PipelineStartTask/ConnectionUtils.ts
+++ Tasks/PipelineStartTask/ConnectionUtils.ts
@@ -1,10 +1,36 @@
-import {WebApi} from "azure-devops-node-api";
 import * as azdev from "azure-devops-node-api";
+import {IRequestOptions} from "azure-devops-node-api/interfaces/common/VsoBaseInterfaces";
+import * as tl from 'azure-pipelines-task-lib/task';
 
 export class ConnectionUtils {
-    public static async getAzureDevopsConnection(token: string, orgUrl: string): Promise<WebApi> {
+    public static async getAzureDevopsConnection(token: string, orgUrl: string): Promise<azdev.WebApi> {
         let authHandler = await azdev.getPersonalAccessTokenHandler(token);
         let connection = await new azdev.WebApi(orgUrl, authHandler);
         return connection;
+    }
+
+    public static  getWebApiWithProxy(serviceUri: string, accessToken?: string): azdev.WebApi {
+        if (!accessToken) {
+            accessToken = this.getSystemAccessToken();
+        }
+
+        const credentialHandler = azdev.getBasicHandler('vsts', accessToken);
+        const options: IRequestOptions = {
+            proxy: tl.getHttpProxyConfiguration(serviceUri),
+            allowRetries: true,
+            maxRetries: 5
+        };
+        return new azdev.WebApi(serviceUri, credentialHandler, options);
+    }
+
+    private static getSystemAccessToken(): string {
+        tl.debug('Getting credentials for local feeds');
+        const auth = tl.getEndpointAuthorization('SYSTEMVSSCONNECTION', false);
+        if (auth.scheme === 'OAuth') {
+            tl.debug('Got auth token');
+            return auth.parameters['AccessToken'];
+        } else {
+            tl.warning('Could not determine credentials to use');
+        }
     }
 }
--- Tasks/PipelineStartTask/dto/test_results/TestResultsAPITest.ts
+++ Tasks/PipelineStartTask/dto/test_results/TestResultsAPITest.ts
@@ -1,5 +1,6 @@
 import {TestResultsBuilder} from "./TestResultsBuilder";
 import {ConnectionUtils} from "../../ConnectionUtils";
+import {WebApi} from "azure-devops-node-api";
 
 let projectName = 'demo-app';
 
@@ -8,9 +9,11 @@ const token = 'fzhzniawld2wh524y2h2sft2ksm23nanspwk6blg4lxhegirixcq';
 const orgUrl = 'https://dev.azure.com/evgenelokshin';
 let buildId: number = 34;
 
-ConnectionUtils.getAzureDevopsConnection(token, orgUrl).then(con => {
-    TestResultsBuilder.getTestsResultsByBuildId(con, projectName, buildId, 'sreverId', 'jobId').then(res => {
-        console.log('######################### finished ###############################');
-        console.log(res);
-    })
+let api: WebApi = ConnectionUtils.getWebApiWithProxy(orgUrl, token);
+
+TestResultsBuilder.getTestsResultsByBuildId(api, projectName, buildId, 'sreverId', 'jobId').then(res => {
+    console.log('######################### finished ###############################');
+    console.log(res);
 });
+
+
