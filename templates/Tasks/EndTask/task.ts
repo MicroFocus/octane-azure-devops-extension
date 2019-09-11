a4a5398a2b8d7ef22798d91c23ee68d14e0428f7
stekel <moshe.stekel@microfocus.com>
2019-09-11 15:25:33 +0300

    ignore tsling over task templates

--- templates/Tasks/EndTask/task.ts
+++ templates/Tasks/EndTask/task.ts
@@ -1,7 +1,10 @@
+// @ts-ignore
 import { EndTask } from './EndTask';
+// @ts-ignore
 import tl = require('azure-pipelines-task-lib');
 
 
+// @ts-ignore
 async function runTask() {
     let endTask: EndTask = await EndTask.instance(tl);
     await endTask.run();
--- templates/Tasks/StartTask/task.ts
+++ templates/Tasks/StartTask/task.ts
@@ -1,7 +1,10 @@
+// @ts-ignore
 import { StartTask } from './StartTask';
+// @ts-ignore
 import tl = require('azure-pipelines-task-lib');
 
 
+// @ts-ignore
 async function runTask() {
     let startTask: StartTask = await StartTask.instance(tl);
     await startTask.run();
