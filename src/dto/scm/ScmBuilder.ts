d5c7f69447145e76efede9af40b796a9406463cf
stekel <moshe.stekel@microfocus.com>
2019-12-22 14:21:08 +0200

    multi pipeline fixes

--- src/dto/scm/ScmBuilder.ts
+++ src/dto/scm/ScmBuilder.ts
@@ -10,66 +10,8 @@ import {LogUtils} from "../../LogUtils";
 
 
 export class ScmBuilder {
-
     public static async buildScmData(connection: WebApi, projectName: string, toBuild: number, logger: LogUtils): Promise<ScmData> {
-        function convertType(changeType: number): string {
-            return VersionControlChangeType[changeType].toLowerCase();
-        }
-
-        let scmCommit = new Array<ScmCommit>();
-        let fromBuild: number = toBuild - 1;
-        let buildApi: ba.IBuildApi = await connection.getBuildApi();
-        let gitApi: git.IGitApi = await connection.getGitApi();
-        let repos = await gitApi.getRepositories(projectName);
-        if(!repos || repos.length == 0) {
-            logger.info('No local repositories found. SCM information could not be collected');
-            return null;
-        }
-        let repo = repos[0];
-        let prev_build = await buildApi.getBuild(projectName, fromBuild);
-        let changes_between_builds;
-        if (prev_build == null) {
-            changes_between_builds =await buildApi.getBuildChanges(projectName, toBuild);
-        } else {
-            try {
-                changes_between_builds = await buildApi.getChangesBetweenBuilds(projectName, fromBuild, toBuild);
-            } catch (e) {
-                changes_between_builds = await buildApi.getBuildChanges(projectName, toBuild);
-            }
-        }
-
-        if (!changes_between_builds || !changes_between_builds.length) {
-            logger.info('No changes were found between builds [' + fromBuild + '] and [' + toBuild + ']');
-            return null;
-        }
-        let scmData: ScmData;
-        let type = changes_between_builds[0].type;
-        let scmRepo = new ScmRepository(type, repo.webUrl, repo.defaultBranch);
-        logger.debug('Changes between builds [' + fromBuild + '] and [' + toBuild + ']');
-        logger.debug(changes_between_builds);
-        for (let change of changes_between_builds) {
-            let time = new Date(change.timestamp).getTime();
-            let comment = change.message;
-            let user = change.author.displayName;
-            let user_email = change.author.uniqueName;
-            let commit = await gitApi.getCommit(change.id, repo.id, projectName);
-            let revId = commit.commitId;
-            let parentRevId = commit.parents[0];
-            let changes = await gitApi.getChanges(revId, repo.id, projectName);
-            let realChanges = changes.changes.filter((element) =>
-                element.item.isFolder == null);
-            let fileChanges = new Array<ScmCommitFileChange>();
-            for (let realChange of realChanges) {
-                let type: string = convertType(realChange.changeType);
-                fileChanges.push(new ScmCommitFileChange(type, realChange.item.path));
-            }
-            scmCommit.push(new ScmCommit(time, user, revId, fileChanges, user_email, parentRevId, comment));
-        }
-        let build = await buildApi.getBuild(projectName, toBuild);
-        scmData = new ScmData(scmRepo, build.buildNumberRevision, scmCommit);
-        logger.info("ScmData was created");
-        logger.debug(scmData);
-        return scmData;
+        return null;
     }
 }
 
