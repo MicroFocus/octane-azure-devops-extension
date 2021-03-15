06c2ed91e20eed89eefc791e282ed084c0156edb
andrei.busmachiu <77609520+andrei-b-mf@users.noreply.github.com>
2021-03-15 16:41:31 +0200

    Added the whole solution with the refactor.

---
+++ src/ExtensionConstants.ts
@@ -0,0 +1,28 @@
+export const enum CI_SERVER_INFO {
+    CI_SERVER_TYPE = 'azure_devops',
+    PLUGIN_VERSION = '1.0.0'
+}
+
+export enum InputConstants {
+    OCTANE_SERVICE_CONNECTION = 'OctaneServiceConnection',
+    GITHUB_REPOSITORY_CONNECTION = 'GithubRepositoryConnection',
+    WORKSPACES_LIST = 'WorkspaceList'
+}
+
+export enum SystemVariablesConstants {
+    SYSTEM_TEAM_FOUNDATION_COLLECTION_URI = 'System.TeamFoundationCollectionUri',
+    SYSTEM_TEAM_PROJECT_ID = 'System.TeamProjectId',
+    SYSTEM_TEAM_PROJECT = 'System.TeamProject',
+    ALM_OCTANE_LOG_LEVEL = 'ALMOctaneLogLevel',
+    BUILD_SOURCE_BRANCH_NAME = 'Build.SourceBranchName',
+    BUILD_DEFINITION_NAME = 'Build.DefinitionName',
+    BUILD_BUILD_ID = 'Build.BuildId',
+    AGENT_JOB_NAME = 'Agent.JobName',
+    AGENT_JOB_STATUS = 'AGENT_JOBSTATUS'
+}
+
+export enum EndpointDataConstants {
+    ENDPOINT_DATA_OCTANE_INSTANCE_ID = 'ENDPOINT_DATA_Octane_INSTANCE_ID',
+    ENDPOINT_DATA_OCTANE_AZURE_PERSONAL_ACCESS_TOKEN = "ENDPOINT_DATA_Octane_AZURE_PERSONAL_ACCESS_TOKEN",
+    ENDPOINT_URL = 'ENDPOINT_URL'
+}
---
+++ src/debug/TaskDbg.ts
@@ -0,0 +1,73 @@
+import * as fs from 'fs';
+const TOML = require('@ltd/j-toml');
+import {StartTask} from '../StartTask';
+import {EndTask} from "../EndTask";
+import {BaseTask} from "../BaseTask";
+import {DebugConf, DebugConfToDebugMapsConverter} from "./debug-conf";
+import {AzurePipelineTaskLibMock, initAzureTaskMock} from "./task-mock-initializer";
+import {InputConstants, SystemVariablesConstants} from "../ExtensionConstants";
+import {EndpointAuthorization} from "azure-pipelines-task-lib";
+
+let azureTaskMock: AzurePipelineTaskLibMock = <AzurePipelineTaskLibMock>{};
+let conf: DebugConf;
+
+function initDebugConfFromInputParametersFile() {
+    let confFilePath: string = process.argv[2];
+    confFilePath = confFilePath.split('=')[1];
+
+    let buffer = fs.readFileSync(confFilePath);
+    let confData = TOML.parse(buffer, 1.0, '\n', false);
+    console.log(confData);
+
+    conf = DebugConfToDebugMapsConverter.convert(confData);
+}
+
+function printNodeVersion() {
+    let envNodeVersion = azureTaskMock.execSync(`node`, `--version`);
+    console.log('Env node version: ' + envNodeVersion.stdout);
+
+    let processArgNodeVersion = process.argv[0];
+    if(processArgNodeVersion.includes("node.exe")) {
+        console.log('Process argv node version: ' + processArgNodeVersion);
+    }
+}
+
+function printOctaneServiceConnectionDetails() {
+    let octaneServiceConnection = azureTaskMock.getInput(InputConstants.OCTANE_SERVICE_CONNECTION);
+    let endpointAuth: EndpointAuthorization = azureTaskMock.getEndpointAuthorization(octaneServiceConnection);
+
+    console.log('Octane service connection: ' + octaneServiceConnection);
+    console.log('Authentication scheme: ' + endpointAuth.scheme);
+    console.log('Parameters: ' + endpointAuth.parameters);
+}
+
+function initialize() {
+    initDebugConfFromInputParametersFile();
+    initAzureTaskMock(azureTaskMock, conf);
+    printNodeVersion();
+    printOctaneServiceConnectionDetails();
+}
+
+async function runTasks() {
+    conf.systemVariables.set(SystemVariablesConstants.AGENT_JOB_NAME, BaseTask.ALM_OCTANE_PIPELINE_START);
+    let startTask: StartTask = await StartTask.instance(azureTaskMock);
+    await startTask.run();
+
+    conf.systemVariables.set(SystemVariablesConstants.AGENT_JOB_NAME, 'C');
+    let startInnerTask: StartTask = await StartTask.instance(azureTaskMock);
+    await startInnerTask.run();
+
+    conf.systemVariables.set(SystemVariablesConstants.AGENT_JOB_NAME, 'C');
+    conf.systemVariables.set(SystemVariablesConstants.AGENT_JOB_STATUS, 'Failed');
+    let endInnerTask: EndTask = await EndTask.instance(azureTaskMock);
+    await endInnerTask.run();
+
+    conf.systemVariables.set(SystemVariablesConstants.AGENT_JOB_NAME, BaseTask.ALM_OCTANE_PIPELINE_END);
+    conf.systemVariables.set(SystemVariablesConstants.AGENT_JOB_STATUS, 'Succeeded');
+    let endTask: EndTask = await EndTask.instance(azureTaskMock);
+    await endTask.run();
+}
+
+initialize();
+runTasks().catch(err => console.error(err));
+
