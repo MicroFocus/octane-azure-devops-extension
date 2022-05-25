1f12b183ec1457c32d1691d54805a5cde5ce23bd
NShitrit <nissim.shitrit2@microfocus.com>
2022-05-25 14:25:20 +0300

    user story #1654024:Send parameter values that pipeline run with on start and finish event and it will be shown Octane

--- src/services/pipelines/PipelineParametersService.ts
+++ src/services/pipelines/PipelineParametersService.ts
@@ -12,6 +12,41 @@ export class PipelineParametersService {
         this.logger = logger;
     }
 
+    public async getParameters(connection: WebApi, definitionId: number, buildId: string, projectName: string, branchName: string): Promise<CiParameter[]> {
+        try {
+            const buildApi: ba.IBuildApi = await connection.getBuildApi();
+            const buildDef = await buildApi.getDefinition(projectName, definitionId);
+            let parameters: CiParameter[] = [this.createParameter('branch', '', true, branchName)];
+            this.logger.info('Get parameters from task.');
+            this.logger.debug('Get Parameters - Build definition variables: ' + buildDef?.variables?.toString());
+            if (buildDef.variables) {
+                Object.keys(buildDef.variables)
+                        .map(paramKey =>this.createParameter(paramKey, '', true,this.tl.getVariable(paramKey) ? this.tl.getVariable(paramKey) : ''))
+                        .forEach(parameter => parameters.push(parameter));
+            }
+
+            this.logger.info('Get parameters from build.');
+            const build = await buildApi.getBuild(projectName, Number(buildId));
+            this.logger.debug('Build parameters received: ' + build.parameters);
+            if (build.parameters) {
+                const buildParams = JSON.parse(build.parameters);
+                Object.keys(buildParams).forEach(paramKey => {
+                    const variable = buildParams[paramKey];
+                    if (!parameters.some(parameter => parameter.name === paramKey)) {
+                        parameters.push(this.createParameter(paramKey, '', true,variable))
+                    }
+                });
+            }
+            if(parameters?.length > 0) {
+                this.logger.info('Build parameters: ' + parameters.map(param => param.name + '=' + param.defaultValue + '-'+ param.value).join(';'));
+            }
+            return parameters;
+        } catch (e) {
+            this.logger.error('Failed to parse parameters. error: ' + e);
+        }
+        return [];
+    }
+
     public async getDefinedParameters(connection: WebApi, definitionId: number, projectName: string, branchName: string): Promise<CiParameter[]> {
         const buildApi: ba.IBuildApi = await connection.getBuildApi();
         let parameters: CiParameter[] = [this.createParameter('branch', branchName, undefined, 'Branch to execute pipeline')];
