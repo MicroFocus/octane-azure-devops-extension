2946ee4bcecdd502c28e39935bd5c459663bf655
NShitrit <nissim.shitrit2@microfocus.com>
2022-07-13 14:41:59 +0300

    defect #1874096:check node version should be optional and not stop the execution

--- src/NodeUtils.ts
+++ src/NodeUtils.ts
@@ -2,8 +2,12 @@ import {LogUtils} from "./LogUtils";
 
 export class NodeUtils {
     public static outputNodeVersion(tl: any, logger: LogUtils) {
-        let result = tl.execSync(`node`, `--version`);
-        logger.info('Machine node version: ' + result.stdout);
+        try {
+            let result = tl.execSync(`node`, `--version`);
+            logger.info('Machine node version: ' + result.stdout);
+        } catch (e) {
+            logger.error('Failed to get agent node js version error: ' + e );
+        }
         logger.info('Actual process.version(Agent node version in Azure DevOps): ' + process.version);
     }
 }
